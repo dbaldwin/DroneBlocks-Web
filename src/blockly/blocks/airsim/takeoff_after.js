d74d28465eaec635e698bc6ea5746012fa436050
Dennis Baldwin <dennisbaldwin@gmail.com>
2020-11-24 09:36:56 -0600

    Takeoff after with next statement

---
+++ src/blockly/blocks/airsim/takeoff_after.js
@@ -0,0 +1,21 @@
+Blockly.Blocks['takeoff_after'] = {
+    /**
+     * Show block.
+     * @this Blockly.Block
+     */
+    init: function() {
+      this.jsonInit(
+        {
+          "message0": "takeoff after %1 seconds",
+          "args0": [
+            {
+                "type": "input_value",
+                "name": "delay"
+            }
+          ],
+          "previousStatement": true,
+          "nextStatement": true,
+          "colour": "#264653"
+        });
+    }
+  };
